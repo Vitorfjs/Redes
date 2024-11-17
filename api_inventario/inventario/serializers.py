8cb85acf68d060b4725264e58f0e4f5d9e6b590e
Vitor <vitor.acad@hotmail.com>
2024-11-17 18:08:16 -0300

    Corrigindo serializer de inventario

--- api_inventario/inventario/serializers.py
+++ api_inventario/inventario/serializers.py
@@ -7,20 +7,13 @@ class SupplierSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class InventoryItemSerializer(serializers.ModelSerializer):
-    supplier_info = SupplierSerializer()
+    # Representação do fornecedor
+    supplier_info = SupplierSerializer(read_only=True)
+    # Criação/atualização do fornecedor por ID
+    supplier = serializers.PrimaryKeyRelatedField(
+        queryset=Supplier.objects.all(), write_only=True, source='supplier_info'
+    )
 
     class Meta:
         model = InventoryItem
         fields = '__all__'
-
-    def create(self, validated_data):
-        # Extrair dados do fornecedor
-        supplier_data = validated_data.pop('supplier_info', None)
-
-        # Criar o fornecedor, se os dados existirem
-        if supplier_data:
-            supplier = Supplier.objects.create(**supplier_data)
-            validated_data['supplier'] = supplier
-
-        # Criar o item de inventário
-        return InventoryItem.objects.create(**validated_data)
